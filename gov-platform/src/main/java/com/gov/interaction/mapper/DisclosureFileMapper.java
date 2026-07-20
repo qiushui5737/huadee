ab3abc8050690e5b35cbd3003d0b42e63ed5bf42
qiushui5737 <2676922827@qq.com>
2026-07-20 18:19:32 +0800

    Revert "feat: 依申请公开、政民互动模块完善/SQL脚本整合"

--- gov-platform/src/main/java/com/gov/interaction/mapper/DisclosureFileMapper.java
+++
@@ -1,9 +0,0 @@
-package com.gov.interaction.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.gov.interaction.entity.DisclosureFile;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DisclosureFileMapper extends BaseMapper<DisclosureFile> {
-}
